962f6b22f8cd451497522e60fddd85a2b349a3c3
sara54321 <srutti@student.ethz.ch>
2025-04-15 08:41:55 +0200

    Update FoDS_Project_code.py

--- FoDS_Project_code.py
+++ FoDS_Project_code.py
@@ -1,1 +1,32 @@
 #Hi
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+import warnings
+warnings.filterwarnings("ignore")
+
+data = pd.read_csv(
+    "oral_cancer_prediction_dataset.csv",
+    dtype={
+        "Country": "category",
+        "Gender": "category",
+        "Tobacco Use": "category",
+        "Alcohol Consumption": "category",
+        "HPV Infection": "category",
+        "Betel Quid Use": "category",
+        "Chronic Sun Exposure": "category",
+        "Poor Oral Hygiene": "category",
+        "Diet (Fruits & Vegetables Intake)": "category",
+        "Family History of Cancer": "category",
+        "Compromised Immune System": "category",
+        "Oral Lesions": "category",
+        "Unexplained Bleeding": "category",
+        "Difficulty Swallowing": "category",
+        "White or Red Patches in Mouth": "category",
+        "Cancer Stage": "category", 
+        "Treatment Type": "category",
+        "Early Diagnosis": "category", 
+        "Oral Cancer (Diagnosis)": "category"
+    }
+)
